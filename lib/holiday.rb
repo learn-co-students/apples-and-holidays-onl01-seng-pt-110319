2ca4945f1753ee71cc4735d0358a65308d44c3ab
Henry Phan <h.phan06@gmail.com>
2019-11-14 18:08:56 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  binding.pry
   holiday_hash[:winter].values.flatten
   
   # holiday_hash[:winter].each do |winter_holiday, value|
